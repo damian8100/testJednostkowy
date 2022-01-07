737d74c041cc12a3b70f7da2ccff6ce59a587f2b
Damian <damian8100@gmail.com>
2022-01-07 20:24:09 +0100

    Module 8.2 try! catch! finally!

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,32 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+
+
+        try{
+            System.out.println("Result " + firstChallenge.divide(3,0));
+        } catch (ArithmeticException e) {
+            System.out.println("Oh no! Dzieliłeś przez zero?? " + e);
+        } finally {
+            System.out.println("I am gonna be here... always!");
+        }
+
+
+
+    }
+}
--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/Class.java
+++
@@ -1,4 +0,0 @@
-package com.kodilla.stream.immutable;
-
-public class Class {
-}
