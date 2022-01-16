673956245010fa7c422654e4f2924aa50e52401d
Damian <damian8100@gmail.com>
2022-01-16 18:35:34 +0100

    Module 8.4 Zadanie: Wiedza o wyjtkach

--- kodilla-exception/src/test/java/com/kodilla/exception/test/FindFlight.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/FindFlight.java
@@ -6,17 +6,19 @@ public class FindFlight {
 
  public void findFlight(Flight flight)throws RouteNotFoundException{
 
+
      HashMap<String, Boolean>theFlightList = new HashMap<>();
      theFlightList.put("Warszawa", true);
      theFlightList.put("Wrocław",true);
      theFlightList.put("Katowice",false);
 
-     if(theFlightList.containsKey(flight.getDeparatureAirport())){
+     boolean cityExistsInMap = theFlightList.containsKey(flight.getDeparatureAirport());
+     if(!cityExistsInMap){ // Wykrzyknik robi zaprzeczenie.
          throw new RouteNotFoundException("Lotnisko nie istnieje");
      }
-     if(theFlightList.containsKey(flight.getArrivalAirport())){
-         throw new RouteNotFoundException("Lotnisko nie istnieje");
-     }
+    if(!theFlightList.get(flight.getDeparatureAirport())){// z list flightlist jesli istnieje a ma wartość false równiez wyrzucan7y jest wyjątek
+        throw new RouteNotFoundException("Lotnisko nie istnieje");
+    }
 
  }
 }
