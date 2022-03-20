da4c4bc5cdc62edf831071976f26364a49915732
Damian <damian8100@gmail.com>
2022-03-20 12:41:08 +0100

    Zadanie 17.4 Zadanie: wyszukujemy pracownikow oraz firmy wedlug zadanych kryteriow

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -0,0 +1,111 @@
+package com.kodilla.hibernate.manytomany.dao;
+
+import com.kodilla.hibernate.manytomany.Company;
+import com.kodilla.hibernate.manytomany.Employee;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CompanyDaoTestSuite {
+
+    @Autowired
+    private CompanyDao companyDao;
+    private EmployeeDao employeeDao;
+
+    @Test
+    void testSaveManyToMany() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        dataMaesters.getEmployees().add(stephanieClarckson);
+        dataMaesters.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmith);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        //When
+        companyDao.save(softwareMachine);
+        int softwareMachineId = softwareMachine.getId();
+        companyDao.save(dataMaesters);
+        int dataMaestersId = dataMaesters.getId();
+        companyDao.save(greyMatter);
+        int greyMatterId = greyMatter.getId();
+
+        //Then
+        assertNotEquals(0, softwareMachineId);
+        assertNotEquals(0, dataMaestersId);
+        assertNotEquals(0, greyMatterId);
+
+        //CleanUp
+        //try {
+        //    companyDao.deleteById(softwareMachineId);
+        //    companyDao.deleteById(dataMaestersId);
+        //    companyDao.deleteById(greyMatterId);
+        //} catch (Exception e) {
+        //    //do nothing
+        //}
+    }
+    @Test
+    void getLastNameTest(){
+        List<Employee>createListBeforeAddEmployer = employeeDao.getLastName("Smith");
+        int beforeSize = createListBeforeAddEmployer.size();
+        Employee johnSmith = new Employee("John", "Smith");
+        employeeDao.save(johnSmith);
+        List<Employee>getLastName = employeeDao.getLastName("Smith");
+
+
+        assertEquals(beforeSize+1,getLastName.size());
+    }
+    @Test
+    void firstThreeCharOfNameTest(){
+
+       //given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        dataMaesters.getEmployees().add(stephanieClarckson);
+        dataMaesters.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmith);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        //when
+        List<Company>createListBeforeAddCompany = companyDao.firstThreeCharOfName("Sof");
+        int beforeSizeOfCompany = createListBeforeAddCompany.size();
+        companyDao.save(softwareMachine);
+       List<Company>firstThreeCharOfName = companyDao.firstThreeCharOfName("Sof");
+
+        //then
+
+       assertEquals(beforeSizeOfCompany+1,firstThreeCharOfName);
+
+    }
+}
