3377898f33cb76a15fe5673bc1b815781eb92ee4
Damian <damian8100@gmail.com>
2022-08-05 15:12:26 +0200

    Modul 30.3 Zadanie: wywoanie procedury UpdateBestsellers z poziomu Javy

---
+++ src/main/java/com/kodilla/testing2/google/config/WebDriverConfig.java
@@ -0,0 +1,24 @@
+package com.kodilla.testing2.google.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverConfig {
+
+    public final static String FIREFOX = "FIREFOX DRIVER";
+    public final static String CHROME = "CHROME DRIVER";
+    public final static WebDriver getDriver(final String driver) {
+        System.setProperty("webdriver.gecko.driver", "C:\\Selenium-drivers\\Firefox\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", "C:\\Selenium-drivers\\Chrome\\chromedriver.exe");
+
+        if(driver.equals(FIREFOX)){
+            return  new FirefoxDriver();
+        }else if (driver.equals(CHROME)) {
+            return new ChromeDriver();
+        }else {
+            return null;
+        }
+
+    }
+}
